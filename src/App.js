170392f4f684a3fbe19cd8f7dc50762d31262b93
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-10-26 01:31:46 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TodoList from './ToDo/TodoList'
+
+function App() {
+  return (
+    <div className='wrapper'>
+      <h1>React Tutorial</h1>
+
+      <TodoList></TodoList>
+
+    </div>
+  )
+}
+
+export default App;

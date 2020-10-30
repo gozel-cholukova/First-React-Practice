c96f8368c75479eafb82ae681d16b1873b844141
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-10-31 00:53:51 +0500

    Add files via upload

--- src/ToDo/TodoItem.js
+++ src/ToDo/TodoItem.js
@@ -17,22 +17,26 @@ const styles = {
   }
 }
 
-function TodoItem({ todo, index }) {
+function TodoItem({ todo, index, onChange }) {
+  console.log('todo', todo)
   return (
     <li style={styles.li}>
       <span>
-        <input type='checkbox' styles={styles.input}/> 
+        <input type='checkbox' styles={styles.input} 
+        onChange={() => onChange(todo.id)} /> 
         <strong>{index + 1}</strong>
+        &nbsp;
         {todo.title}
       </span>
       <button className='rm'>&times;</button>
     </li>
   )
-}
+} 
 
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
-  index: PropTypes.number
+  index: PropTypes.number,
+  onChange: PropTypes.func.isRequired
 }
 
 export default TodoItem

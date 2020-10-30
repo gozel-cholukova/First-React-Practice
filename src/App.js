b90c43ef12ab0a30630bc1ed7b272fc8e7c33909
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-10-31 00:55:18 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import React from 'react'
 import TodoList from './ToDo/TodoList'
 
 function App() {
-  const todos = [
+  let todos = [
     {id: 1 , completed: false, title: 'bread'},
     {id: 2 , completed: false, title: 'oil'},
     {id: 3 , completed: false, title: 'milk'},
   ]
+
+
+  function toggleTodo(id) {
+    todos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo 
+    })
+  }
+  
   return (
     <div className='wrapper'>
       <h1>React Tutorial</h1>
 
-      <TodoList todos={todos}></TodoList>
+      <TodoList 
+      todos={todos}
+      onToggle={toggleTodo}>
+
+      </TodoList>
 
     </div>
   )

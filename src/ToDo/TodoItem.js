b74c6e9e2fbed8be72edbf2fd41f467538766240
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-11-14 00:15:21 +0500

    Add files via upload

--- src/ToDo/TodoItem.js
+++ src/ToDo/TodoItem.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
+import Context from '../context'
 
 
 const styles = {
@@ -18,17 +19,26 @@ const styles = {
 }
 
 function TodoItem({ todo, index, onChange }) {
-  console.log('todo', todo)
+  const { removeTodo } = useContext(Context)
+  const classes = []
+
+  if (todo.completed) {
+    classes.push('done')
+  }
+
   return (
     <li style={styles.li}>
-      <span>
-        <input type='checkbox' styles={styles.input} 
+      <span className={classes.join(' ')}>
+        <input 
+        type='checkbox' 
+        checked={todo.completed}
+        styles={styles.input} 
         onChange={() => onChange(todo.id)} /> 
         <strong>{index + 1}</strong>
         &nbsp;
         {todo.title}
       </span>
-      <button className='rm'>&times;</button>
+      <button className='rm' onClick={removeTodo.bind(null, todo.id)}>&times;</button>
     </li>
   )
 } 

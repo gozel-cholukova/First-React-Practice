d704ac4374b96b765d9b52f8584790ae233ea761
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-11-19 23:06:05 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,34 +1,62 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TodoList from './ToDo/TodoList'
+import Context from './context'
+import AddTodo from './AddTodo'
 
 function App() {
-  const q = React.useState ([
+  const [todos, setTodos] = React.useState ([
     {id: 1 , completed: false, title: 'bread'},
-    {id: 2 , completed: false, title: 'oil'},
+    {id: 2 , completed: true, title: 'oil'},
     {id: 3 , completed: false, title: 'milk'},
   ])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTodos(todos)
+      })
+  }, [])
   
   function toggleTodo(id) {
-    todos = todos.map(todo => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo 
-    })
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo 
+      })
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      completed: false
+    }]))
   }
   
   return (
-    <div className='wrapper'>
-      <h1>React Tutorial</h1>
-
-      <TodoList 
-      todos={todos}
-      onToggle={toggleTodo}>
-
-      </TodoList>
-
-    </div>
+    <Context.Provider value={{ removeTodo }}>
+      <div className='wrapper'>
+        <h1>React Tutorial</h1>
+        <AddTodo onCreate={addTodo}></AddTodo>
+        {todos.length ? (
+          <TodoList 
+          todos={todos}
+          onToggle={toggleTodo}>
+          </TodoList>
+        ) : (
+          <p>No todos!</p> 
+        )}        
+      </div>
+    </Context.Provider>
   )
 }
 
-export default App;
+export default App
